2addf9b2476155ca3d166057d43d914c307ef2c3
soumenshit1810-tech <soumenshit1810@gmail.com>
2025-09-25 22:53:09 +0530

    update

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-pink-200 via-pink-300 to-pink-400 dark:from-gray-800 dark:via-gray-900 dark:to-black shadow-lg mt-12">
+    <footer className="bg-gradient-to-r from-pink-200 via-pink-300 to-pink-400 dark:from-gray-800 dark:via-gray-900 dark:to-black shadow-lg mt-0">
       <div className="max-w-screen-xl mx-auto px-8 py-10 md:flex md:items-center md:justify-between">
         {/* Logo + Brand */}
         <div className="flex items-center space-x-3">
